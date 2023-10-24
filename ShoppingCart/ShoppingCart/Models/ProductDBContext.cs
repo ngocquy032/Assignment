7b70efbf304dd0e3bd57f9b8ad740c5101246c48
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2023-10-24 22:52:05 +0700

    ShoppingCart

---
+++ ShoppingCart/ShoppingCart/Models/ProductDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingCart.Models
+{
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext()
+        {
+        }
+
+        public ProductDBContext(DbContextOptions<ProductDBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-QG7K8HCB\\SQLEXPRESS;Initial Catalog=T2208M_Movie;Persist Security Info=True;User ID=sa;Password=241403;Encrypt=False");
+        }
+
+        public DbSet<Product> Products { get; set; }
+      
+    }
+}
